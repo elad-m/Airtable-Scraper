f4b4faef84c4e0a00f14e9ff6e2a9257669a8527
elad-m <musbae7@gmail.com>
2019-09-10 20:50:08 +0300

    Kinda scrolling and retrieving lines successfully along the thable rows

--- sel-web-reader.py
+++ sel-web-reader.py
@@ -7,53 +7,59 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver.common.touch_actions import TouchActions
-
+from selenium.webdriver.common.action_chains import ActionChains
 import sys
 
 
 options = Options()  # this is here because it didn't work the easy way
 options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-browser  = webdriver.Chrome(chrome_options=options, executable_path=r"C:\Users\Elad\AppData\Local\chromedriver\chromedriver.exe", )
+driver  = webdriver.Chrome(chrome_options=options, executable_path=r"C:\Users\Elad\AppData\Local\chromedriver\chromedriver.exe", )
 
-browser.get("https://airtable.com/shrl5EIxGUExC3umi/tblvOwxPcPVcFmwxt?blocks=hide")
+driver.get("https://airtable.com/shrl5EIxGUExC3umi/tblvOwxPcPVcFmwxt?blocks=hide")
 
 try:
-    # browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    element = WebDriverWait(browser, 10).until(EC.presence_of_element_located
-    ((By.CLASS_NAME, "antiscroll-scrollbar")))
-    browser.maximize_window()
-    list_of_obj = browser.find_elements_by_class_name("url")
-    print(len(list_of_obj))
     
-    scrollbar_style = browser.find_element_by_class_name(
-    "antiscroll-scrollbar-vertical").get_attribute("style")
-    print(scrollbar_style)
+    driver.maximize_window()
+    for px_to_scroll in range(0, 383, 9):
+        # wait for scrollbar and scroll by px_to_scroll
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located
+                                       ((By.CLASS_NAME, "antiscroll-scrollbar-vertical")))
+        # scrollbar = driver.find_element_by_class_name("antiscroll-scrollbar-vertical")
+        ActionChains(driver).move_to_element(driver.find_element_by_class_name("antiscroll-scrollbar-vertical")
+               ).drag_and_drop_by_offset(driver.find_element_by_class_name(
+               "antiscroll-scrollbar-vertical"), 0, px_to_scroll).perform()
     
+        
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located
+        ((By.CLASS_NAME, "numberText")))
+        list_of_obj = driver.find_elements_by_class_name("numberText")
+        row_num_string = ""
+        for i in range(len(list_of_obj)):
+            row_num_string += list_of_obj[i].text + ", "
+        print(row_num_string, "END")
+        # print("len: ", len(list_of_obj))
+       
+        driver.refresh()
     
-    list_of_obj = browser.find_elements_by_class_name("url")
-    print(len(list_of_obj))
-    
-    
-    ## i think scrolling need to use touch and that might not be allowed
-    ## in this site?
-    
-    
+    ## TODO: handle the moveTargetOutOfBound exception
+    ## write to a new file that deletes the previous
+    ## write the urls  and not the numbers
     
 
     
 except Exception:
     print("there was an exception:", 
-    sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
+          sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2], 
+          "LINE: ", sys.exc_info()[2].tb_lineno)
     
  
-browser.quit()
+driver.quit()
 
 
 # print(some_str + "transform: translated3d(0px, , 0px);")
 
   # js = "return document.getElementsByClassName('paneInnerContent')[3].getAttribute('style');"
     
-    # some_str = browser.execute_script(js) 
+    # some_str = driver.execute_script(js) 
     # for i in range(len(list_of_obj)):
         # print(list_of_obj[i].text)
