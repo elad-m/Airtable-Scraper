b225aa75ecd43d6e33a82efcb9efc88b734d1374
elad-m <musbae7@gmail.com>
2019-09-17 00:03:57 +0300

    still trying to find a way for the scrolling to wait for reading the rows. Now from some pointat every scroll it loses more and more rows.

--- sel-web-reader.py
+++ sel-web-reader.py
@@ -19,43 +19,58 @@ driver  = webdriver.Chrome(chrome_options=options, executable_path=r"C:\Users\El
 
 driver.get("https://airtable.com/shrl5EIxGUExC3umi/tblvOwxPcPVcFmwxt?blocks=hide")
 
+
+def get_num_of_records(driver):
+    WebDriverWait(driver, 10, 0.3).until(EC.presence_of_element_located
+                                           ((By.CLASS_NAME, "summaryCell")))
+    summary_cell_webelement = driver.find_element_by_class_name("summaryCell")
+    return summary_cell_webelement.text.split()[0]
+     
+    
+    
 try:
     
     driver.maximize_window()
+    num_of_records = get_num_of_records(driver)  # this would be a string
+    print("number of records: ", num_of_records)
     px_to_scroll = 8
-    for i in range(42):  # 42 scrolling for 9px scroll for a 383px scroll bar
+    row_num_string = ""
+    rows_dic = {}
+    for i in range(int(num_of_records)):
         if i != 0:  # not scrolling the first time
             # wait for scrollbar and scroll by px_to_scroll
-            WebDriverWait(driver, 10, 0.3).until(EC.presence_of_element_located
+            WebDriverWait(driver, 10, 0.1).until(EC.presence_of_element_located
                                            ((By.CLASS_NAME, "antiscroll-scrollbar-vertical")))
             
-            # start_time = time.time()
-            # WebDriverWait(driver, 50, 0.3).until(EC.staleness_of(scrollbar))
-            # end_time = time.time()
-            # print("time: ", end_time - start_time)
             actions = ActionChains(driver)
             scrollbar = driver.find_element_by_class_name("antiscroll-scrollbar-vertical")
-            actions.move_to_element(driver.find_element_by_class_name("antiscroll-scrollbar-vertical"))
-            actions.drag_and_drop_by_offset(driver.find_element_by_class_name(
-                   "antiscroll-scrollbar-vertical"), 0, px_to_scroll).perform()
+            actions.move_to_element(scrollbar)
+            actions.drag_and_drop_by_offset(scrollbar, 0, px_to_scroll).perform()
     
-        
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located
-        ((By.CLASS_NAME, "numberText")))
-        list_of_obj = driver.find_elements_by_class_name("numberText")
-        row_num_string = ""
+        WebDriverWait(driver, 10, 1).until(EC.presence_of_all_elements_located
+        ((By.CLASS_NAME, "numberText"))) # that is the class name of the element of row number 
+        ##todo this wait should be a variable: wait to get the next dataRow
+        pane_inner_content = driver.find_element_by_class_name("paneInnerContent")
+        ## here you need dataLeftPane and right. You have multiple paneInnerContent-s
+        ## because url and row number are on different divs
+        list_of_obj = pane_inner_content.find_elements_by_css_selector("div.dataRow.leftPane")
+        # list_of_obj = driver.find_elements_by_class_name("numberText")
+        temp_str = ""
         for i in range(len(list_of_obj)):
             curr_elem = list_of_obj[i]
             if curr_elem.is_displayed():
-                row_num_string += curr_elem.text + ", "
-        print(row_num_string, "END")
-        # print("len: ", len(list_of_obj))
-       
+                temp_str += curr_elem.text + ", "
+                row_num_string += temp_str
+        # print(temp_str)
         driver.refresh()
+        print(row_num_string[-5:])
     
+    print(row_num_string, "END")
     ## problems:
-    ## -stale element happens on a whim apparently, maybe internet connection? non-deterministic
+    ## not stopping ...
     ## -still scrolling too much - a lot of jobs missing.
+    ## -stale element happens on a whim apparently, maybe internet connection? non-deterministic
+    ## change the condition of the for loop for what it really is
     ## -not able to scroll the page for trials using js console: 
     
     # document.getElementsByClassName('antiscroll-scrollbar-vertical')[0].style.transform = "translate3d(0, 9, 0)";
